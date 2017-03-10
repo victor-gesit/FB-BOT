52138a04ae9fb31a01dea86f1f437e5fe217f3e1
idem <victoridongesit@gmail.com>
2017-03-10 17:05:49 +0100

    Testing Webhook

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', function(req,res){
 // Used for verification
 
 app.get("/webhook", function(req,res){
-	if(req.query['hub.verify_token'] === process.env.VERIFICATION_TOKE){
+	if(req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN){
 		console.log("Verified webhook");
 		res.status(200).send(req.query["hub.challenge"]);
 	} else{
